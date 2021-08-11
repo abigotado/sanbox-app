c51acabc5c37743105910b77161a40f832d13ed8
Abigotado <82471392+Abigotado@users.noreply.github.com>
2021-08-11 14:51:16 +0300

    Implement videocall using firebase:
    - import firebase
    - connect firestore database
    - use firestore as signaling
    - code feature of creating rooms and joining rooms
    - enable possibility of making videocalls inside the room

--- lib/widgets/video_call.dart
+++ lib/widgets/video_call.dart
@@ -1,12 +1,11 @@
 import 'dart:core';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sandbox/widgets/signaling.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
 
 class VideoCall extends StatefulWidget {
-  final String host = 'flutter-sandbox-abigotado.herokuapp.com';
-
   VideoCall({Key? key}) : super(key: key);
 
   @override
@@ -14,209 +13,134 @@ class VideoCall extends StatefulWidget {
 }
 
 class _VideoCallState extends State<VideoCall> {
-  Signaling? _signaling;
-  List<dynamic> _peers = [];
-  var _selfId;
+  Signaling signaling = Signaling();
   RTCVideoRenderer _localRenderer = RTCVideoRenderer();
   RTCVideoRenderer _remoteRenderer = RTCVideoRenderer();
-  bool _inCalling = false;
-  Session? _session;
+  String? roomId;
+  TextEditingController textEditingController = TextEditingController(text: '');
 
   @override
   void initState() {
-    super.initState();
-    initRenderers();
-    _connect();
-    print('init' + '$_selfId');
-  }
+    _localRenderer.initialize();
+    _remoteRenderer.initialize();
 
-  initRenderers() async {
-    await _localRenderer.initialize();
-    await _remoteRenderer.initialize();
+    signaling.onAddRemoteStream = ((stream) {
+      _remoteRenderer.srcObject = stream;
+      setState(() {});
+    });
+
+    super.initState();
   }
 
   @override
-  void deactivate() {
-    super.deactivate();
-    if (_signaling != null) _signaling!.close();
+  void dispose() {
     _localRenderer.dispose();
     _remoteRenderer.dispose();
-  }
-
-  void _connect() async {
-    if (_signaling == null) {
-      _signaling = Signaling(widget.host)..connect();
-
-      _signaling!.onSignalingStateChange = (SignalingState state) {
-        switch (state) {
-          case SignalingState.ConnectionClosed:
-          case SignalingState.ConnectionError:
-          case SignalingState.ConnectionOpen:
-            break;
-        }
-        print(_selfId);
-      };
-
-      _signaling!.onCallStateChange = (Session? session, CallState state) {
-        switch (state) {
-          case CallState.CallStateNew:
-            setState(() {
-              _session = session;
-              _inCalling = true;
-            });
-            break;
-          case CallState.CallStateBye:
-            setState(() {
-              _localRenderer.srcObject = null;
-              _remoteRenderer.srcObject = null;
-              _inCalling = false;
-              _session = null;
-            });
-            break;
-          case CallState.CallStateInvite:
-          case CallState.CallStateConnected:
-          case CallState.CallStateRinging:
-        }
-      };
-
-      _signaling!.onPeersUpdate = ((event) {
-        setState(() {
-          _selfId = event['self'];
-          _peers = event['peers'];
-        });
-      });
-
-      _signaling!.onLocalStream = ((_, stream) {
-        _localRenderer.srcObject = stream;
-      });
-
-      _signaling!.onAddRemoteStream = ((_, stream) {
-        _remoteRenderer.srcObject = stream;
-      });
-
-      _signaling!.onRemoveRemoteStream = ((_, stream) {
-        _remoteRenderer.srcObject = null;
-      });
-    }
-  }
-
-  _invitePeer(BuildContext context, String peerId, bool useScreen) async {
-    if (_signaling != null && peerId != _selfId) {
-      _signaling!.invite(peerId, 'video', useScreen);
-    }
-  }
-
-  _hangUp() {
-    if (_signaling != null) {
-      _signaling!.bye(_session!.sid!);
-    }
-  }
-
-  _switchCamera() {
-    _signaling!.switchCamera();
-  }
-
-  _muteMic() {
-    _signaling!.muteMic();
-  }
-
-  _buildRow(context, peer) {
-    var self = (peer['id'] == _selfId);
-    print('row');
-    return ListBody(children: <Widget>[
-      ListTile(
-        title: Text(self
-            ? peer['name'] + '[Your self]'
-            : peer['name'] + '[' + peer['user_agent'] + ']'),
-        onTap: null,
-        trailing: IconButton(
-          icon: const Icon(Icons.videocam),
-          onPressed: () => _invitePeer(context, peer['id'], false),
-          tooltip: 'Video calling',
-        ),
-        subtitle: Text('id: ' + peer['id']),
-      ),
-      Divider()
-    ]);
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    print('build');
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('P2P Call Sample'),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.settings),
-            onPressed: null,
-            tooltip: 'setup',
-          ),
-        ],
-      ),
-      // floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      // floatingActionButton: _inCalling
-      //     ? SizedBox(
-      //         width: 200.0,
-      //         child: Row(
-      //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      //             children: <Widget>[
-      //               FloatingActionButton(
-      //                 child: const Icon(Icons.switch_camera),
-      //                 onPressed: _switchCamera,
-      //               ),
-      //               FloatingActionButton(
-      //                 onPressed: _hangUp,
-      //                 tooltip: 'Hangup',
-      //                 child: Icon(Icons.call_end),
-      //                 backgroundColor: Colors.pink,
-      //               ),
-      //               FloatingActionButton(
-      //                 child: const Icon(Icons.mic_off),
-      //                 onPressed: _muteMic,
-      //               )
-      //             ]))
-      //     : null,
-      // body: _inCalling
-      //     ? OrientationBuilder(builder: (context, orientation) {
-      //         return Container(
-      //           child: Stack(children: <Widget>[
-      //             Positioned(
-      //                 left: 0.0,
-      //                 right: 0.0,
-      //                 top: 0.0,
-      //                 bottom: 0.0,
-      //                 child: Container(
-      //                   margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
-      //                   width: MediaQuery.of(context).size.width,
-      //                   height: MediaQuery.of(context).size.height,
-      //                   child: RTCVideoView(_remoteRenderer),
-      //                   decoration: BoxDecoration(color: Colors.black54),
-      //                 )),
-      //             Positioned(
-      //               left: 20.0,
-      //               top: 20.0,
-      //               child: Container(
-      //                 width: orientation == Orientation.portrait ? 90.0 : 120.0,
-      //                 height:
-      //                     orientation == Orientation.portrait ? 120.0 : 90.0,
-      //                 child: RTCVideoView(_localRenderer, mirror: true),
-      //                 decoration: BoxDecoration(color: Colors.black54),
-      //               ),
-      //             ),
-      //           ]),
-      //         );
-      //       })
-      body: ListView.builder(
-          shrinkWrap: true,
-          padding: const EdgeInsets.all(0.0),
-          itemCount: (_peers.length != null ? _peers.length : 0),
-          itemBuilder: (context, i) {
-            return _buildRow(context, _peers[i]);
-            // return ListTile(
-            //   title: Text('list $_selfId'),
-            // );
-          }),
-    );
+    return Scaffold(body: OrientationBuilder(builder: (context, orientation) {
+      return Container(
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+                left: 0.0,
+                right: 0.0,
+                top: 0.0,
+                bottom: 0.0,
+                child: Container(
+                    margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    color: Colors.white,
+                    child: RTCVideoView(_remoteRenderer))),
+            Positioned(
+              left: 20.0,
+              top: 20.0,
+              child: Container(
+                width: orientation == Orientation.portrait ? 90.0 : 120.0,
+                height: orientation == Orientation.portrait ? 120.0 : 90.0,
+                padding: EdgeInsets.only(left: 10),
+                child: Container(
+                    child: RTCVideoView(_localRenderer, mirror: true)),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(10, 400, 10, 10),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          // Add roomId
+                          signaling.joinRoom(
+                            textEditingController.text,
+                            _remoteRenderer,
+                          );
+                        },
+                        child: Text("Join room"),
+                      ),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      ElevatedButton(
+                        onPressed: () async {
+                          roomId = await signaling.createRoom(_remoteRenderer);
+                          textEditingController.text = roomId!;
+                          setState(() {});
+                        },
+                        child: Text("Create room"),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          signaling.openUserMedia(
+                              _localRenderer, _remoteRenderer);
+                        },
+                        child: Text("Open camera & microphone"),
+                      ),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          signaling.hangUp(_localRenderer);
+                        },
+                        child: Text("Hangup"),
+                      )
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Join the following Room: "),
+                        Flexible(
+                          child: TextFormField(
+                            controller: textEditingController,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+    }));
   }
 }
